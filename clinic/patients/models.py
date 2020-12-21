50d903f90b38b5ff949dba19a8124bf879f8be86
Zhukovvlad <zhukovvlad@yandex.ru>
2020-12-21 18:06:06 +0300

    Added Person model. Before migrations

--- clinic/patients/models.py
+++ clinic/patients/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+from clinic.users.models import User
+
+
+class Patient(TimeStampedModel):
+    name = models.CharField("Patient's name", max_length=255)
+    birth_date = models.DateField(
+        null=True,
+        blank=True
+    )
+    slug = AutoSlugField(
+        always_update=False,
+        populate_from="name"
+    )
+
+    creator = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="created"
+    )
